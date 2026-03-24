b01ebb84573efd7cf0b089d7c11d7fc659dd3aa6
Thiago-gps94 <tgps94@gmail.com>
2026-03-24 16:59:45 -0300

    Limpeza e organização do site

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const toggle = document.getElementById("menu-toggle");
-const menu = document.querySelector(".menu ul");
+const menu = document.getElementById("menu-list");
 const form = document.getElementById("form-contato");
 
 if (toggle && menu) {
   toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
+
+    const expanded = toggle.getAttribute("aria-expanded") === "true";
+    toggle.setAttribute("aria-expanded", String(!expanded));
   });
 }
 
