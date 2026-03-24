a4a60b4e6b90d92ae4e8301d6909ac6f55c09a72
Thiago-gps94 <tgps94@gmail.com>
2026-03-24 15:57:48 -0300

    Atulizção visul do site com nova paleta e logo maior

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const toggle = document.getElementById("menu-toggle");
 const menu = document.querySelector(".menu ul");
-
-toggle.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
-
 const form = document.getElementById("form-contato");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Mensagem enviada com sucesso!");
-})
+if (toggle && menu) {
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+}
+
+if (form) {
+  form.addEventListener("submit", () => {
+    alert("Sua mensagem está sendo enviada.");
+  });
+}
